128c334d681292f47c4b0480bba84c884993a693
Yuheng <yuhengzhang1016@gmail.com>
2023-12-08 12:20:26 -0800

    configure main.dart for amplify

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -9,6 +9,9 @@ import 'plant_info_from_category_page.dart';
 import 'camera_page.dart';
 import 'plant_identification_page.dart';
 import 'my_plants_page.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'amplifyconfiguration.dart';
 
 import 'package:flutter/services.dart'; // Import SystemChrome class
 
@@ -29,8 +32,29 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      final auth = AmplifyAuthCognito();
+      await Amplify.addPlugin(auth);
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      safePrint('An error occurred configuring Amplify: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {

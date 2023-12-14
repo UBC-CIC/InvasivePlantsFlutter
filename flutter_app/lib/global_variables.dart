7a1289acb69b8a90aed680a1a6f0a64b03fdb307
vsaosuo <visal.saosuo@gmail.com>
2023-12-13 16:07:55 -0800

    added global variable file

---
+++ flutter_app/lib/global_variables.dart
@@ -0,0 +1,5 @@
+
+/// Currently selected region
+/// Default, select based on the current regiont or the first region of the regionList[]
+/// Mannual selection, user can switch between regions mannually and the value of this variable update based on selection
+var selectedRegion = {};

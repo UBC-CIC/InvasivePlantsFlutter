79022ad22f1bf3399401c455ff07f03a30dab183
Yuheng <yuhengzhang1016@gmail.com>
2023-12-14 17:07:19 -0800

    fill wiki tab on invasive species

--- flutter_app/lib/plant_info_from_category_invasive_page.dart
+++ flutter_app/lib/plant_info_from_category_invasive_page.dart
@@ -6,6 +6,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'dart:convert'; // Import dart:convert to use utf8 decoding
 
 import 'package:flutter_app/plant_info_from_category_page.dart';
+import 'wiki_webscrape.dart';
 
 class PlantInfoFromCategoryInvasivePage extends StatefulWidget {
   final Map<String, dynamic> speciesObject;
@@ -225,7 +226,7 @@ class _PlantInfoFromCategoryInvasivePageState
                           text: 'About',
                         ),
                         Tab(
-                          text: 'Wiki Info',
+                          text: 'Wikipedia',
                         ),
                         Tab(
                           text: 'Alternatives',
@@ -288,10 +289,10 @@ class _PlantInfoFromCategoryInvasivePageState
                                 ),
                               Padding(
                                 padding:
-                                    const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                                    const EdgeInsets.fromLTRB(15, 10, 15, 0),
                                 child: Text(
                                   speciesDescription,
-                                  style: const TextStyle(fontSize: 18),
+                                  style: const TextStyle(fontSize: 16),
                                 ),
                               ),
                               if (resourceLinks.isNotEmpty)
@@ -308,7 +309,6 @@ class _PlantInfoFromCategoryInvasivePageState
                                             fontWeight: FontWeight.bold,
                                             fontSize: 18),
                                       ),
-                                      const SizedBox(height: 5),
                                       // Generate list of clickable URLs
                                       ...resourceLinks.map(
                                         (link) => GestureDetector(
@@ -336,8 +336,271 @@ class _PlantInfoFromCategoryInvasivePageState
                             ],
                           ),
                         ),
-                        Column(
-                          children: [Container()],
+                        FutureBuilder<Map<String, Object>>(
+                          future: webscrapeWikipedia(scientificName),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Gathering info...',
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  CircularProgressIndicator(),
+                                  SizedBox(
+                                    height: 40,
+                                  ),
+                                ],
+                              ));
+                            } else if (snapshot.hasError) {
+                              return Center(
+                                  child: Text('Error: ${snapshot.error}'));
+                            } else if (!snapshot.hasData ||
+                                snapshot.data!.isEmpty) {
+                              return const Center(
+                                  child: Text('No Wikipedia info available'));
+                            } else {
+                              Map<String, Object> wikiInfo = snapshot.data!;
+                              // Display Wikipedia info
+                              return SingleChildScrollView(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'Introduction',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const Padding(
+                                      padding:
+                                          EdgeInsets.fromLTRB(0, 0, 246, 0),
+                                      child: Divider(
+                                        height: 1,
+                                        thickness: 1,
+                                        color: Colors.green,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      wikiInfo['overview'].toString(),
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                    // Display body if available
+                                    if (wikiInfo['body'] != null &&
+                                        wikiInfo['body'] is List &&
+                                        (wikiInfo['body'] as List).isNotEmpty)
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const SizedBox(height: 20),
+                                          const Text(
+                                            'Overview',
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const Padding(
+                                            padding: EdgeInsets.fromLTRB(
+                                                0, 0, 274, 0),
+                                            child: Divider(
+                                              height: 1,
+                                              thickness: 1,
+                                              color: Colors.green,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 10),
+                                          ...List.generate(
+                                            (wikiInfo['body'] as List).length,
+                                            (index) {
+                                              final body =
+                                                  wikiInfo['body'] as List?;
+                                              if (body != null &&
+                                                  index < body.length) {
+                                                final header = body[index]
+                                                        ?['header'] ??
+                                                    '';
+                                                final bodyContent =
+                                                    body[index]?['body'] ?? '';
+
+                                                // Check if both header and body content are not empty
+                                                if (header.isNotEmpty &&
+                                                    bodyContent.isNotEmpty) {
+                                                  return Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      Text(
+                                                        '$header:',
+                                                        style: const TextStyle(
+                                                          fontSize: 16,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                        ),
+                                                      ),
+                                                      const SizedBox(height: 5),
+                                                      Text(
+                                                        bodyContent,
+                                                        style: const TextStyle(
+                                                            fontSize: 14),
+                                                      ),
+                                                      const SizedBox(
+                                                          height: 10),
+                                                    ],
+                                                  );
+                                                }
+                                              }
+                                              return const SizedBox(); // A placeholder or an empty widget
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                    // Display speciesImages if available
+                                    if (wikiInfo['speciesImages'] != null &&
+                                        (wikiInfo['speciesImages'] as List)
+                                            .isNotEmpty)
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            'Images',
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const Padding(
+                                            padding: EdgeInsets.fromLTRB(
+                                                0, 0, 294, 0),
+                                            child: Divider(
+                                              height: 1,
+                                              thickness: 1,
+                                              color: Colors.green,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 150,
+                                            child: ListView.builder(
+                                              scrollDirection: Axis.horizontal,
+                                              itemCount:
+                                                  (wikiInfo['speciesImages']
+                                                          as List)
+                                                      .length,
+                                              itemBuilder: (context, index) {
+                                                final speciesImages =
+                                                    wikiInfo['speciesImages']
+                                                        as List?;
+                                                if (speciesImages != null &&
+                                                    index <
+                                                        speciesImages.length) {
+                                                  return GestureDetector(
+                                                    onTap: () {
+                                                      showDialog(
+                                                        context: context,
+                                                        builder: (_) => Dialog(
+                                                          child:
+                                                              GestureDetector(
+                                                            onTap: () {
+                                                              Navigator.of(
+                                                                      context)
+                                                                  .pop();
+                                                            },
+                                                            child:
+                                                                Image.network(
+                                                              speciesImages[
+                                                                  index],
+                                                              fit: BoxFit.cover,
+                                                            ),
+                                                          ),
+                                                        ),
+                                                      );
+                                                    },
+                                                    child: Padding(
+                                                      padding: const EdgeInsets
+                                                          .fromLTRB(5, 5, 5, 5),
+                                                      child: ClipRRect(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(10),
+                                                        child: Image.network(
+                                                          speciesImages[index],
+                                                          width: 150,
+                                                          height: 150,
+                                                          fit: BoxFit.cover,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  );
+                                                } else {
+                                                  return Container();
+                                                }
+                                              },
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    // Display wikiUrl as a clickable link
+                                    if (wikiInfo['wikiUrl'] != null)
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const SizedBox(height: 20),
+                                          const Text(
+                                            'Link',
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const Padding(
+                                            padding: EdgeInsets.fromLTRB(
+                                                0, 0, 321, 0),
+                                            child: Divider(
+                                              height: 1,
+                                              thickness: 1,
+                                              color: Colors.green,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 5),
+                                          GestureDetector(
+                                            onTap: () async {
+                                              // Open Wikipedia link in the browser
+                                              String url = wikiInfo['wikiUrl']
+                                                  .toString();
+                                              if (await canLaunch(url)) {
+                                                await launch(url);
+                                              } else {
+                                                throw 'Could not launch $url';
+                                              }
+                                            },
+                                            child: Center(
+                                              child: Text(
+                                                wikiInfo['wikiUrl']!.toString(),
+                                                style: const TextStyle(
+                                                  color: Colors.blue,
+                                                  decoration:
+                                                      TextDecoration.underline,
+                                                  fontSize: 14,
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    const SizedBox(height: 20),
+                                  ],
+                                ),
+                              );
+                            }
+                          },
                         ),
                         Column(
                           children: [

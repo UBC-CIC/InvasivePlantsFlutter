84c60dc649020674cfd93a381b6c5c1eff055994
Yuheng <yuhengzhang1016@gmail.com>
2023-12-19 19:05:34 -0800

    remove image variable from plant list notifier

--- flutter_app/lib/plant_list_notifier.dart
+++ flutter_app/lib/plant_list_notifier.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
 class PlantListNotifier extends ChangeNotifier {
-  late List<String> plants = List.generate(10, (index) => 'Plant ${index + 1}');
-  late String _imageUrl = 'assets/images/leaf.png';
-  late int _itemCount = 10;
-  late String listId; // Add listId property
-  late String listName; // Add listName property
+  late List<String> plants = [];
 
-  // late String imageUrl = 'assets/images/leaf.png';
-  // late int itemCount = 10;
-  String get imageUrl => _imageUrl;
+  late int _itemCount = 0;
+  late String listId;
+  late String listName;
+
   int get itemCount => _itemCount;
 
   void removePlant(int index) {
@@ -17,11 +14,6 @@ class PlantListNotifier extends ChangeNotifier {
       plants.removeAt(index);
       notifyListeners();
     }
-  }
-
-  void setImageUrl(String newImageUrl) {
-    _imageUrl = newImageUrl;
-    notifyListeners();
   }
 
   void setItemCount(int newItemCount) {
